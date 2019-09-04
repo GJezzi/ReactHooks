7e250884a0d7f4b596c7d2be419dc3836022ce5d
GJezzi <gjezzi@hotmail.com>
2019-09-04 16:22:43 -0300

    React Hooks Intro

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [techs, setTech] = useState([]);
+    const [newTech, setNewTech] = useState('');
+
+    const handleAddTech = useCallback(() => {
+        setTech([...techs, newTech]);
+        setNewTech('');
+    }, [techs, newTech]);
+
+    useEffect(() => {
+        const storedTech = localStorage.getItem('techs');
+
+        if (storedTech) {
+            setTech(JSON.parse(storedTech));
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('techs', JSON.stringify(techs));
+    }, [techs]);
+
+    const techSize = useMemo(() => techs.length, [techs]);
+
+    return (
+        <>
+            <ul>
+                {techs.map(tech => (
+                    <li key={tech}>{tech}</li>
+                ))}
+            </ul>
+            <strong>Você possui {techSize} tecnologias.</strong>
+            <br />
+            <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+            <button type="button" onClick={handleAddTech}>
+                Adicionar
+            </button>
+        </>
+    );
 }
 
 export default App;
